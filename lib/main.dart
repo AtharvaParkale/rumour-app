44cfd29492590fdd794b368e4475458f4b26e013
atharvaparkale <atharva.parkale@gmail.com>
2026-04-08 01:14:56 +0530

    :memo: add firestore set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseFirestore.instance
+      .collection('test')
+      .add({'name': 'Atharva'});
   runApp(const MyApp());
 }
 
